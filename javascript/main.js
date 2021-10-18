968c90b74d42476d284b0005b6af2239dd5a6236
gyan-js <gyan0304@gmail.com>
2021-10-18 17:17:45 +0530

    Initial Code Drop

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,19 @@
 document.getElementById('about').onclick = function() {
-    window.open("https://resumake.io")
+    window.open("about:blank", "_self")
 }
 
-document.getElementById('sourc').onclick = function() {
-    window.open("https://resumake.io")
+document.getElementById('source').onclick = function() {
+    window.open("https://github.com/gyan-js/resu-generate", "_self")
 }
 
-document.getElementById('about').onclick = function() {
-    window.open("https://resumake.io")
+document.getElementById('issue').onclick = function() {
+    window.open("https://github.com/gyan-js/resu-generate/issues", "_self")
 }
 
-document.getElementById('about').onclick = function() {
-    window.open("https://resumake.io")
+document.getElementById('github').onclick = function() {
+    window.open("https://github.com/gyan-js", "_self")
+}
+
+document.getElementById('contact').onclick = function() {
+    window.open("mailto:gyanshresth03@gmail.com", "_self");
 }
