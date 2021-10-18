6d04338fe92258b64788464052cc44ff395c910a
gyan-js <gyan0304@gmail.com>
2021-10-18 17:12:22 +0530

    Initial code drop

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+document.getElementById('about').onclick = function() {
+    window.open("https://resumake.io")
+}
+
+document.getElementById('sourc').onclick = function() {
+    window.open("https://resumake.io")
+}
+
+document.getElementById('about').onclick = function() {
+    window.open("https://resumake.io")
+}
+
+document.getElementById('about').onclick = function() {
+    window.open("https://resumake.io")
+}
