8056bb0fcfdfd83eecc628e805e1a30c8632badb
unknown <gyanshresth03@gmail.com>
2021-10-19 13:47:23 +0530

    Made the generating screen and started making some templates

--- javascript/main.js
+++ javascript/main.js
@@ -17,3 +17,12 @@ document.getElementById('github').onclick = function() {
 document.getElementById('contact').onclick = function() {
     window.open("mailto:gyanshresth03@gmail.com", "_self");
 }
+
+function switchToResumeScreen() {
+    document.querySelector('footer').style.display = 'none';
+    document.getElementById('home_button').style.display = 'none';
+    document.getElementById('Logo').style.display = 'none'
+    document.querySelector('aside').style.display = 'block';
+    document.querySelector('header').style.display = 'block'
+    //document.getElementById('Logo').style.fontSize = '2em'
+}
